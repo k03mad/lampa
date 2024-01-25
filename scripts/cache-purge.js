9f68f020f3f3e37d59115b1dad4210c3b5424efd
k03mad <k03.mad@gmail.com>
2024-01-25 17:35:59 +0300

    update jsdelivr

--- scripts/cache-purge.js
+++ scripts/cache-purge.js
@@ -1,12 +1,44 @@
+import fs from 'node:fs/promises';
+
 import {request} from '@k03mad/request';
+import {logErrorExit} from '@k03mad/simple-log';
 import {globby} from 'globby';
 
-const PUBLIC_DIR = './public';
+const PLUGINS_DIR = './plugins';
+const STORE_CONFIG = `${PLUGINS_DIR}/store.json`;
 
-const PURGE_URL = 'https://purge.jsdelivr.net/gh/k03mad/lampa/';
+const CDN_DOMAIN = 'https://cdn.jsdelivr.net';
+const PURGE_DOMAIN = 'https://purge.jsdelivr.net';
+const PURGE_PATH = '/gh/k03mad/lampa/';
 const PURGE_RPS = 1;
 
-const purgeRequest = file => request(PURGE_URL + file, {}, {rps: PURGE_RPS});
+const [
+    myPluginsFiles,
+    storeConfig,
+] = await Promise.all([
+    globby(PLUGINS_DIR),
+    fs.readFile(STORE_CONFIG, {encoding: 'utf8'}),
+]);
 
-const files = await globby(PUBLIC_DIR);
-await Promise.all(files.map(purgeRequest));
+const myCdnUrls = myPluginsFiles
+    .map(link => PURGE_DOMAIN + PURGE_PATH + link);
+
+if (myCdnUrls.length === 0) {
+    logErrorExit(`Plugins CDN urls not found: ${PLUGINS_DIR}`);
+}
+
+const storeCdnUrls = JSON
+    .parse(storeConfig)
+    .results
+    .flatMap(({results}) => results.map(({link}) => link.replace(CDN_DOMAIN, PURGE_DOMAIN)))
+    .filter(link => link.includes(PURGE_DOMAIN));
+
+if (storeCdnUrls.length === 0) {
+    logErrorExit(`Store CDN urls not found: ${PLUGINS_DIR}`);
+}
+
+const urls = new Set([myCdnUrls, storeCdnUrls].flat());
+
+await Promise.all([...urls].map(
+    url => request(url, {}, {rps: PURGE_RPS}),
+));
