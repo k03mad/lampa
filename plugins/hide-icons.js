9004f9a6df7c5dd0043f8e163e3cc58367ca87ec
k03mad <k03.mad@gmail.com>
2024-01-25 16:29:59 +0300

    remove iife

--- plugins/hide-icons.js
+++ plugins/hide-icons.js
@@ -1,18 +1,16 @@
-(() => {
-    const ITEMS_HIDE_SELECTORS = [
-        '.open--premium',
-        '.open--feed',
-    ];
+const ITEMS_HIDE_SELECTORS = [
+    '.open--premium',
+    '.open--feed',
+];
 
-    const main = () => ITEMS_HIDE_SELECTORS.forEach(item => $(item).hide());
+const main = () => ITEMS_HIDE_SELECTORS.forEach(item => $(item).hide());
 
-    if (window.appready) {
-        main();
-    } else {
-        Lampa.Listener.follow('app', event => {
-            if (event.type === 'ready') {
-                main();
-            }
-        });
-    }
-})();
+if (window.appready) {
+    main();
+} else {
+    Lampa.Listener.follow('app', event => {
+        if (event.type === 'ready') {
+            main();
+        }
+    });
+}
